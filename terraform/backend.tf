4c5fb2468e57f5d744509cacde05e06d9f75e3c7
Tayyab-Nisar <41720678+Tayyab-Nisar@users.noreply.github.com>
2026-04-24 21:09:50 +0400

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatebackend12345"
-    container_name       = "tfstate"
-    key                  = "devopsapp.terraform.tfstate"
-  }
-}
